1a0101c7ad9bd881b00bbc7a57bf7b6d2c01b250
Riccardo Scuto <riccardoscuto.96@gmail.com>
2024-01-12 17:19:10 +0100

    Multi feed selection

--- src/Components/MultiFeed.jsx
+++ src/Components/MultiFeed.jsx
@@ -1,9 +1,40 @@
 import { Button, Checkbox, CheckboxGroup, Image, Card, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Stack, useDisclosure } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useWrite } from "../hook/useWrite";
 
 
 export default function MultiFeed({ mokupInfo }) {
+    const [arraySelected, setArraySelected] = useState([]);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { write, error, prepareError, isError, isPrepareError } = useWrite({
+        abi: [{
+            "inputs": [],
+            "name": "unpause",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }],
+        address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        args: [],
+        enabled: true,
+        functionName: "unpause",
+        value: BigInt(0)
+    });
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    useEffect(() => {
+        setArraySelected(Array(mokupInfo.length).fill(false))
+    },
+        [])
+
+    const submit =() => {write?.();}
+    const selectOnePlant = (index) => {
+        const aux = arraySelected;
+        aux[index] = !aux[index];
+        setArraySelected([...aux])
+    }
+    const selectAll = () => {
+        setArraySelected([...arraySelected.map((e) => { return true })])
+    };
 
     return (
         <>
@@ -14,22 +45,30 @@ export default function MultiFeed({ mokupInfo }) {
                     <ModalHeader>Choose the plants to feed</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <CheckboxGroup colorScheme='green' >
-                            <Stack spacing={[1, 3]} direction={['row', 'column']}>
-                                {
-                                    mokupInfo && mokupInfo.map((element, index) => {
-                                        return (
-                                            <Card key={index} padding={2} >
-                                                <Checkbox value={element.text} >
-                                                    <Image width="40px" src={element.img} />
-                                                    {element.text + " " + element.info[0]}
-                                                </Checkbox>
-                                            </Card>
-                                        )
-                                    })
-                                }
-                            </Stack>
-                        </CheckboxGroup>
+
+                        <Stack spacing={[1, 3]} direction={['row', 'column']}>
+                            <Card key={-1} padding={2} >
+                                <Button onClick={selectAll} >
+                                    Select All
+                                </Button>
+                                <Button onClick={submit}>
+                                    Submit
+                                </Button>
+                            </Card>
+                            {
+                                mokupInfo && arraySelected.length > 0 && mokupInfo.map((element, index) => {
+                                    console.log("selected", arraySelected[index]);
+                                    return (
+                                        <Card key={index} padding={2} >
+                                            <Checkbox value={element.text} isChecked={arraySelected[index]} onChange={() => { selectOnePlant(index) }}  >
+                                                <Image width="40px" src={element.img} />
+                                                {element.text + " " + element.info[0]}
+                                            </Checkbox>
+                                        </Card>
+                                    )
+                                })
+                            }
+                        </Stack>
                     </ModalBody>
                 </ModalContent>
             </Modal>
--- src/hook/useWrite.js
+++ src/hook/useWrite.js
@@ -1,23 +1,23 @@
 import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi"
 
-export function useWrite(address, abi, functionName, args, enabled, value) {
-	const {
-		config,
-		error: prepareError,
-		isError: isPrepareError,
-	} = usePrepareContractWrite({
-		address: address,
-		abi: abi,
-		functionName: functionName,
-		args: args,
-		enabled: enabled,
-		value: value
-	})
-	const { data, error, isError, write } = useContractWrite(config)
+export function useWrite({ address, abi, functionName, args, enabled, value }) {
+    const {
+        config,
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
+        address: address,
+        abi: abi,
+        functionName: functionName,
+        args: args,
+        enabled: enabled,
+        value: value
+    })
+    const { data, error, isError, write } = useContractWrite(config)
 
-	const { isLoading, isSuccess } = useWaitForTransaction({
-		hash: data?.hash,
-	})
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data ? data.hash : undefined,
+    })
 
-	return { prepareError, isPrepareError, error, isError, write, isLoading, isSuccess, data }
+    return { prepareError, isPrepareError, error, isError, write, isLoading, isSuccess, data }
 }
